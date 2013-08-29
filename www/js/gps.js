5cbe0f34265e08f2e69ca148bc3863b9743f0594
Mirco Bertelli <mircobe87@gmail.com>
2013-08-29 16:47:34 +0200

    corretti alcuni bug relativi alla geolocalizzazione

--- www/js/gps.js
+++ www/js/gps.js
@@ -1,17 +1,17 @@
 var lat = document.getElementById("gps.content.lat");
 var lon = document.getElementById("gps.content.lon");
 
-function geolocationSuccess(posizione){
+function geolocationSuccess(position){
 	lat.innerHTML = position.coords.latitude;
 	lon.innerHTML = position.coords.longitude;
 }
 
-function geolocationError(errore){
+function geolocationError(error){
 	alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
 }
 
 function gpsUpdate(){
-	navigator.geolocation.getCurrentPosition(geolocationSuccess,geolocationError);
+	navigator.geolocation.getCurrentPosition(geolocationSuccess,geolocationError,{enableHighAccuracy: true});
 }
 
 gpsUpdate();
