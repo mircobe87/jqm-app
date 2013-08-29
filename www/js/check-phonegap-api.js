9ee011e54c3c91a7c90d384fc95ff0679ea72e41
Mirco Bertelli <mircobe87@gmail.com>
2013-08-29 12:28:26 +0200

    aggiunto controllo evento "deviceready"

---
+++ www/js/check-phonegap-api.js
@@ -0,0 +1,5 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	var img = document.getElementById("mainpage.content.apiicon");
+	img.src="img/ok-32.png";
+}
